63f2e365e9a9332421660341b31a302dfc047df9
GabGian <gabriele.giannini@isendu.com>
2021-01-20 17:17:25 +0100

    implementing the bigrams method

--- main.cpp
+++ main.cpp
@@ -22,20 +22,59 @@ string fileToString(string file)
     return sTotal;
 }
 
+int charLenght(char ch)
+{
+    int test = ch & 0xE0;
+    int charLenght = 0;
+    if (test < 128)
+    {
+        charLenght = 1;
+    }
+    else if (test < 224)
+    {
+        charLenght = 2;
+    }
+    else if (test < 240)
+    {
+        charLenght = 3;
+    }
+    else
+    {
+        charLenght = 4;
+    }
+    return charLenght;
+}
+
+pair<string, int> compileBigram(string file, int initPos)
+{
+    string b = "";
+    int test = file[initPos];
+    int charLen = charLenght(file[initPos]);
+    for (int i = initPos; i < charLen + initPos; i++)
+    {
+        b.push_back(file[i]);
+    }
+    int charLen2 = charLenght(file[charLen + initPos]);
+    for (int i = charLen + initPos; i < charLen + charLen2 + initPos; i++)
+    {
+        b.push_back(file[i]);
+    }
+    return pair<string, int>(b, charLen + initPos);
+}
+
 unordered_map<string, int> bigrams(string file)
 {
-    string b = "  ";
-    b[0] = file[0];
-    b[1] = file[1];
+    pair<string, int> p = compileBigram(file, 0);
+    string b = p.first;
     vector<string> bigrams(1);
     bigrams[0] = b;
     unordered_map<string, int> map;
     map[b] = 1;
-    for (int i = 1; i < file.length() - 2; i++)
+    for (int i = p.second; i < file.length() - 2;)
     {
-        string a = "  ";
-        a[0] = file[i];
-        a[1] = file[i + 1];
+        pair<string, int> p2 = compileBigram(file, i);
+        string a = p2.first;
+        i = p2.second;
         bool exists = false;
         for (int j = 0; j < bigrams.size(); j++)
         {
@@ -117,12 +156,18 @@ int main()
 {
     string fToString;
     fToString = fileToString("example.txt");
-    future<unordered_map<string, int>> fut = async(launch::async, bigrams, fToString.substr(0, fToString.length() / 2));
-    future<unordered_map<string, int>> fut2 = async(launch::async, bigrams, fToString.substr((fToString.length() / 2 - 1), (fToString.length() - fToString.length() / 2 + 1)));
+    future<unordered_map<string, int>> fut = async(launch::async, bigrams, fToString.substr(0, fToString.length() / 4));
+    future<unordered_map<string, int>> fut2 = async(launch::async, bigrams, fToString.substr((fToString.length() / 4 - 1), fToString.length() / 4 + 1));
+    future<unordered_map<string, int>> fut3 = async(launch::async, bigrams, fToString.substr((fToString.length() - ((2 * fToString.length()) / 4) - 1), fToString.length() / 4 + 1));
+    future<unordered_map<string, int>> fut4 = async(launch::async, bigrams, fToString.substr((fToString.length() - ((3 * fToString.length()) / 4) - 1), fToString.length() / 4 + 1));
     unordered_map<string, int> futArr1 = fut.get();
     unordered_map<string, int> futArr2 = fut2.get();
-    unordered_map<string, int> futArr3 = mergeMap(futArr1, futArr2);
-    for (pair<string, int> p : futArr3)
+    unordered_map<string, int> futArr3 = fut3.get();
+    unordered_map<string, int> futArr4 = fut4.get();
+    unordered_map<string, int> futMap1 = mergeMap(futArr1, futArr2);
+    unordered_map<string, int> futMap2 = mergeMap(futArr3, futArr4);
+    unordered_map<string, int> futMapFin = mergeMap(futMap1, futMap2);
+    for (pair<string, int> p : futMapFin)
     {
         cout << p.first << " " << p.second << endl;
     }
