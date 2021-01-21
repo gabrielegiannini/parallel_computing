3fd0deee5e37d56d251d5bd9bc295b7e2b05d627
Cosimo Cinquilli <riceberg.5lli@gmail.com>
2021-01-22 00:47:57 +0100

    Better file splitting. Implementing generic n-gram computation. Better command line options (-t and -n)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <unordered_map>
 #include <future>
 #include <filesystem>
-#include <cstring>
 
 namespace fs = std::filesystem;
 
@@ -13,7 +12,7 @@ namespace fs = std::filesystem;
 
 using namespace std;
 
-string fileToString(const string & file)
+string fileToString(const string &file)
 {
     string s;
     string sTotal;
@@ -48,47 +47,54 @@ int charLenght(char ch)
     return charLenght;
 }
 
-pair<string, int> compileBigram(string file, int initPos)
+//make groups of n characters
+pair<string, int> compileNgram(int n, const string &file, int initPos)
 {
-    string b = "";
+    string b;
     int charLen = charLenght(file[initPos]);
     for (int i = initPos; i < charLen + initPos; i++)
     {
         b.push_back(file[i]);
     }
-    int charLen2 = charLenght(file[charLen + initPos]);
-    for (int i = charLen + initPos; i < charLen + charLen2 + initPos; i++)
+    int offset = charLen;
+    int newLen;
+    for (int j = 1; j < n && offset + initPos < file.length(); j++)
     {
-        b.push_back(file[i]);
+        newLen = charLenght(file[offset + initPos]);
+        for (int i = offset + initPos; i < offset + newLen + initPos; i++)
+        {
+            b.push_back(file[i]);
+        }
+        offset += newLen;
     }
     return pair<string, int>(b, charLen + initPos);
 }
 
-unordered_map<string, int> bigrams(string file, int id)
+unordered_map<string, int> ngrams(int n, const string &file, int id)
 {
-    pair<string, int> p = compileBigram(file, 0);
+    pair<string, int> p = compileNgram(n, file, 0);
     string b = p.first;
-    vector<string> bigrams(1);
-    bigrams[0] = b;
+    vector<string> ngrams(1);
+    ngrams[0] = b;
     unordered_map<string, int> map;
     map[b] = 1;
-    for (int i = p.second; i < file.length() - 2;)
+    for (int i = p.second; i < file.length() - n;)
     {
-        pair<string, int> p2 = compileBigram(file, i);
+        pair<string, int> p2 = compileNgram(n, file, i);
         string a = p2.first;
         i = p2.second;
         bool exists = false;
-        for (int j = 0; j < bigrams.size(); j++)
+        for (int j = 0; j < ngrams.size(); j++)
         {
-            if (a == bigrams[j])
+            if (a == ngrams[j])
             {
                 exists = true;
                 break;
             }
         }
-        if (exists == false)
+        if (!exists)
         {
-            bigrams.push_back(a);
+            ngrams.push_back(a);
             map[a] = 1;
         } else
         {
@@ -96,42 +102,6 @@ unordered_map<string, int> bigrams(string file, int id)
         }
     }
     return map;
-}
-
-void trigrams(string file)
-{
-    string b = "   ";
-    b[0] = file[0];
-    b[1] = file[1];
-    b[2] = file[2];
-    vector<string> trigrams(1);
-    trigrams[0] = b;
-    unordered_map<string, int> map;
-    map[b] = 1;
-    for (int i = 1; i < file.length() - 3; i++)
-    {
-        string a = "   ";
-        a[0] = file[i];
-        a[1] = file[i + 1];
-        a[2] = file[i + 2];
-        bool exists = false;
-        for (int j = 0; j < trigrams.size(); j++)
-        {
-            if (a == trigrams[j])
-            {
-                exists = true;
-                break;
-            }
-        }
-        if (exists == false)
-        {
-            trigrams.push_back(a);
-            map[a] = 1;
-        } else
-        {
-            map[a] = map[a] + 1;
-        }
-    }
 }
 
 unordered_map<string, int> mergeMap(unordered_map<string, int> futArr1, unordered_map<string, int> futArr2)
@@ -151,27 +121,25 @@ unordered_map<string, int> mergeMap(unordered_map<string, int> futArr1, unordere
     return futArr2;
 }
 
-vector<future<unordered_map<string, int>>> splitFile(string file, int splits)
+vector<future<unordered_map<string, int>>> splitFile(string file, unsigned int splits, int n)
 {
     vector<future<unordered_map<string, int>>> res(0);
     int adjustment = 0;
     unsigned long lengthFrac = file.length() / splits;
-    // il resto per aggiustare il numero di caratteri (eventuali caratteri in più finiscono nel primo subFile)
-    unsigned long remainder = file.length() % splits;
-    string subFile = file.substr(0, lengthFrac + 1 + remainder);
+    string subFile = file.substr(0, lengthFrac + 1);
 
     /* se il primo byte di file dopo l'ultimo incluso in subFile
      * (che è lengthFrac + 1, perché ho chiesto sottostringa da 0 lunga lengthFrac + 1, quindi va da 0 a lengthFrac)
      * inizia per 10xxxxxx (in UTF-8) allora
      * è un pezzo di un altro carattere spezzato che inizia in subFile. Riaggiungiamolo in subFile e spostiamo il
      * "cursore" di uno avanti*/
-    while ((file[lengthFrac + 1 + adjustment + remainder] & 0xC0) == 128)
+    while ((file[lengthFrac + 1 + adjustment] & 0xC0) == 128)
     {
-        subFile.push_back(file[lengthFrac + 1 + adjustment + remainder]);
+        subFile.push_back(file[lengthFrac + 1 + adjustment]);
         adjustment++;
     }
-    res.push_back(async(launch::async, bigrams, subFile, 0));
-    for (int i = splits - 1; i > 0; i--)
+    res.push_back(async(launch::async, ngrams, n, subFile, splits));
+    for (long i = splits - 1; i > 1; i--)
     {
         unsigned long pos = file.length() - (i * lengthFrac) - 1 + adjustment;
         /* ora riscorriamo all'indietro per tornare all'inizio dell'ultimo carattere messo in subFile (che dovrà
@@ -189,33 +157,62 @@ vector<future<unordered_map<string, int>>> splitFile(string file, int splits)
             subFile.push_back(file[pos + lengthFrac + 1 + adjustment]);
             adjustment++;
         }
-        res.push_back(async(launch::async, bigrams, subFile, i));
+        res.push_back(async(launch::async, ngrams, n, subFile, i));
     }
+    unsigned int pos = file.length() - lengthFrac - 1 + adjustment;
+    while ((file[pos] & 0xC0) == 128)
+    {
+        pos--;
+    }
+    // tutto il resto del file
+    subFile = file.substr(pos, 2 * lengthFrac);
+    res.push_back(async(launch::async, ngrams, n, subFile, 1));
     return res;
 }
 
 int main(int argc, char *argv[])
 {
+    int n = 2;
     unsigned int numThreads = THREADS;
-    /* si può passare al programma il numero di thread da avviare, default 4, oopure HW per indicare che deve
-     * usare un n di thread in base all'hardware del pc (numThread = n° di thread della cpu)
+    /* si può passare al programma il numero di thread da avviare, default 4, oopure "hw" per indicare che deve
+     * usare un num di thread in base all'hardware del pc (numThread = n° di thread della cpu)
+     * si può passare anche n, ovvero la grandezza degli n-grammi da calcolare
      */
-    if (argc == 2)
+    int i = 1;
+    while (i < argc)
     {
-        if (strcmp(argv[1], "HW") == 0 || strcmp(argv[1], "hw") == 0)
-        {
-            numThreads = thread::hardware_concurrency();
-        } else
+        const string token = string(argv[i]);
+        if (token == "-t")
         {
             try
             {
-                numThreads = stoi(argv[1]);
+                numThreads = stoi(argv[++i]);
             } catch (invalid_argument &ex)
             {
-                cerr << "il parametro passato non è un numero valido di threads" << endl;
+                if (string(argv[i]) == "hw")
+                {
+                    numThreads = thread::hardware_concurrency();
+                } else
+                {
+                    cerr << "il parametro passato non è un numero valido di threads" << endl;
+                }
             }
+        } else if (token == "-n")
+        {
+            try
+            {
+                n = stoi(argv[++i]);
+            } catch (invalid_argument &ex)
+            {
+                cerr << "il parametro passato non è un numero valido" << endl;
+            }
+        } else
+        {
+            cerr << "opzione " << argv[i] << " non riconosciuta" << endl;
         }
+        i++;
     }
+    cout << "computing " << n << "-gram with " << numThreads << " threads." << endl;
     string fToString;
     fs::create_directory("output");
     const fs::path inputP{"analyze"};
@@ -223,8 +220,7 @@ int main(int argc, char *argv[])
     {
         cout << "Put all the .txt files to be analyzed in an 'analyze' directory:" << endl;
         cout << "nothing to analyze!" << endl;
-    }
-    else
+    } else
     {
 
         for (const auto &entry: fs::directory_iterator("./analyze"))
@@ -235,7 +231,7 @@ int main(int argc, char *argv[])
                 continue;
             }
             fToString = fileToString(path);
-            vector<future<unordered_map<string, int>>> futures = splitFile(fToString, numThreads);
+            vector<future<unordered_map<string, int>>> futures = splitFile(fToString, numThreads, n);
             unordered_map<string, int> map = futures[0].get();
             for (int i = 1; i < numThreads; i++)
             {
@@ -244,12 +240,11 @@ int main(int argc, char *argv[])
             ofstream outFile;
             const string outPath = "analysis-" + path.stem().string() + ".csv";
             outFile.open(fs::path("output/" + outPath));
-            outFile << "Bigram\tOccurrencies" << endl;
+            outFile << n << "-gram\tOccurrencies" << endl;
             for (pair<string, int> p : map)
             {
                 outFile << p.first << "\t" << p.second << endl;
             }
         }
-        cout << "run with " << numThreads << " threads." << endl;
     }
 }
