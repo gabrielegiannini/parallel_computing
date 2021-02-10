98976ae1dddfd5cdbfde049f07c6a643b3bb0de0
Cosimo Cinquilli <riceberg.5lli@gmail.com>
2021-02-10 14:39:46 +0100

    Prove sul kmeans, dataset più o meno reale

--- kmeans/src/KmeansND.java
+++ kmeans/src/KmeansND.java
@@ -1,34 +1,53 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class KmeansND {
     public static void main(String[] args) throws Exception {
-        BufferedReader csv = new BufferedReader(new FileReader("datasetProva.csv"));
+        BufferedReader csv = null;
+        try {
+            csv = new BufferedReader(new FileReader(args[0]));
+        } catch (FileNotFoundException e) {
+            System.err.println("given path does not exists");
+            System.exit(1);
+        }
         String row = csv.readLine();
         Map<String, Double[]> data = new HashMap<>();
-        int n = 0;
+        double[] domainMax = null;
+        int n = -1;
         while (row != null) {
             String[] rowArr = row.split(";");
-            n = rowArr.length - 1;
+            if (n == -1) {
+                n = rowArr.length - 1;
+                domainMax = new double[n];
+                for (int i = 0; i < n; i++) {
+                    domainMax[i] = Double.MIN_VALUE;
+                }
+            }
             Double[] dataArr = new Double[n];
             for (int i = 1; i < rowArr.length; i++) {
                 dataArr[i - 1] = Double.parseDouble(rowArr[i]);
+                if (dataArr[i - 1] > domainMax[i - 1]) {
+                    domainMax[i - 1] = dataArr[i - 1];
+                }
             }
             data.put(rowArr[0], dataArr);
             row = csv.readLine();
         }
-        int clustersNumber = Integer.parseInt("10"); // args[0]
+        for (Double[] vector : data.values()) {
+            for (int i = 0; i < n; i++) {
+                vector[i] = vector[i] / domainMax[i]; // normalizza i dati -> tutto è adesso fra 0 e 1
+            }
+        }
+        int clustersNumber = Integer.parseInt(args[1]); // args[0]
         double[][] initialMeans = new double[clustersNumber][n];
         Random r = new Random();
         for (int i = 0; i < initialMeans.length; i++) { // array di dimensione clusterNumber contenente arrays di
-                                                        // dimensione n
-            initialMeans[i] = r.doubles(n, -1, 11).toArray();
+            // dimensione n
+            initialMeans[i] = r.doubles(n).toArray();
         }
         List<String>[] S = kmeans(data, initialMeans);
         List<String>[] F;
@@ -37,7 +56,7 @@ public class KmeansND {
             F = S;
             S = kmeans(data, initialMeans);
         } while (different(F, S));
-        printList(S);
+        System.out.println(formatTable(S));
     }
 
     public static List<String>[] kmeans(Map<String, Double[]> data, double[][] initialMeans) {
@@ -45,11 +64,13 @@ public class KmeansND {
         for (int j = 0; j < S.length; j++) {
             S[j] = new ArrayList<>();
         }
-        for (Entry<String, Double[]> entry : data.entrySet()) {// questo dovrà farlo per le n dimensioni dell'input
+        int cont = 0;
+        for (Entry<String, Double[]> entry : data.entrySet()) {
             int posMin = 0;
-            double min = Double.MAX_VALUE;// max value di double e questo sarà un vettore
+            double min = Double.MAX_VALUE;
             for (int h = 0; h < initialMeans.length; h++) {
                 double norm = norm(entry.getValue(), initialMeans[h]);
+                cont++;
                 if (norm < min) {
                     min = norm;
                     posMin = h;
@@ -57,6 +78,7 @@ public class KmeansND {
             }
             S[posMin].add(entry.getKey());
         }
+//        System.out.println("norm calcolata " + cont + " volte");
         return S;
     }
 
@@ -71,13 +93,78 @@ public class KmeansND {
         return no;
     }
 
-    public static void printList(List<String>[] S) {
-        for (int f = 0; f < S.length; f++) {
-            for (String key : S[f]) {
+    public static void printList(Iterable<String>[] S) {
+        for (Iterable<String> strings : S) {
+            for (String key : strings) {
                 System.out.print(key + ", ");
             }
             System.out.println(".");
         }
+    }
+
+    public static void printAnyThing(Object S) {
+        if (S instanceof Object[]) {
+            Object[] oArr = (Object[]) S;
+            for (Object o : oArr) {
+                printAnyThing(o);
+                System.out.print(",");
+            }
+            System.out.println(".");
+            System.out.println();
+        } else if (S instanceof Iterable) {
+            Iterable<Object> oIter = (Iterable<Object>) S;
+            for (Object o : oIter) {
+                printAnyThing(o);
+                System.out.print(",");
+            }
+            System.out.println(".");
+            System.out.println();
+        } else {
+            System.out.print(S.toString());
+        }
+    }
+
+    public static String formatTable(Object o) {
+        StringBuilder sb = new StringBuilder();
+        Formatter form = new Formatter(sb);
+        if (o instanceof Object[]) {
+            Object[] arr = (Object[]) o;
+            List<Object> lt = Arrays.asList(arr);
+            List<List<String>> content = lt.stream().map(KmeansND::mapsObj).collect(Collectors.toList());
+            for (int i = 0; i < arr.length; i++) {
+                form.format("\t%d\t", i);
+            }
+            sb.append("\n");
+            int j = 0;
+            boolean next = true;
+            while (next) {
+                next = false;
+                for (int i = 0; i < arr.length; i++) {
+                    List<String> col = content.get(i);
+                    if (j < col.size()) {
+                        form.format("\t%s\t", col.get(j));
+                        if (j + 1 < col.size()) {
+                            next = true;
+                        }
+                    }
+                }
+                sb.append("\n");
+                j++;
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static List<String> mapsObj(Object o) {
+        List<String> lt = new LinkedList<>();
+        if (o instanceof Iterable) {
+            Iterable<Object> it = (Iterable<Object>) o;
+            for (Object e : it) {
+                lt.add(e.toString());
+            }
+        }
+        return lt;
     }
 
     public static double norm(Double[] a, double[] b) {
