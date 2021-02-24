2a4c703fd8225578e8bbb1e8bf82381a13ea8ded
GabGian <gabriele.giannini@isendu.com>
2021-02-24 20:22:36 +0100

    first try parallelization

--- kmeans/src/KmeansND.java
+++ kmeans/src/KmeansND.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.Map.Entry;
 
-public class KmeansND {
+public class KmeansND extends Thread {
 
     private final int clustersNumber;
     private Map<String, Double[]> data;
@@ -56,7 +56,7 @@ public class KmeansND {
                 totalNormAvg[i] = totalNormAvg[i] / S[i].size();
             }
         }
-//        return S;
+        // return S;
     }
 
     public void means() {
@@ -117,13 +117,16 @@ public class KmeansND {
         for (int i = 0; i < clustersNumber; i++) { // array di dimensione clusterNumber contenente arrays di
             // dimensione n
 
-                /* ok, pagina di wikipedia, sezione "Initialization methods"...non avevamo capito nulla, ahah
-                Quello che facevamo noi non era un metodo sano per inizializzare i means!
-                Su wiki sono spiegati 2 modi: selezionare k vettori a caso tra i dati perché fungano da centroidi
-                iniziali (è quelo che ho fatto io sotto) oppure dividere casualmente tutti i dati fra i cluster e
-                calcolare i means da questa prima suddivisione casuale (wiki dice che i due metodi sono preferibili in
-                circostanze diverse, consideriamo di implementarli entrambi? PS: implementati entrambi ;) )
-                 */
+            /*
+             * ok, pagina di wikipedia, sezione "Initialization methods"...non avevamo
+             * capito nulla, ahah Quello che facevamo noi non era un metodo sano per
+             * inizializzare i means! Su wiki sono spiegati 2 modi: selezionare k vettori a
+             * caso tra i dati perché fungano da centroidi iniziali (è quelo che ho fatto io
+             * sotto) oppure dividere casualmente tutti i dati fra i cluster e calcolare i
+             * means da questa prima suddivisione casuale (wiki dice che i due metodi sono
+             * preferibili in circostanze diverse, consideriamo di implementarli entrambi?
+             * PS: implementati entrambi ;) )
+             */
             int index = r.nextInt(lt.size());
             while (used.contains(index)) {
                 index = r.nextInt(lt.size());
@@ -140,86 +143,23 @@ public class KmeansND {
     public List<String>[] getClusters() {
         return S;
     }
-    
-    public static void main(String[] args) throws Exception {
-        int c = 0;
-        List<String> positionals = new LinkedList<String>();
-        boolean initRandomClusters = false;
-        while (c < args.length) {
-            if ("-initClusters".equals(args[c])) {
-                initRandomClusters = true;
-            } else {
-                positionals.add(args[c]);
-            }
-            c++;
-        }
-        BufferedReader csv = null;
-        try {
-            csv = new BufferedReader(new FileReader(positionals.get(0)));
-        } catch (FileNotFoundException e) {
-            System.err.println("given path does not exists");
-            System.exit(1);
-        }
 
-        String row = csv.readLine();
-        HashMap<String, Double[]> data = new HashMap<>();
-        double[] domainMax = null;
-        int n = -1;
-        while (row != null) {
-            String[] rowArr = row.split(";");
-            if (n == -1) {
-                n = rowArr.length - 1;
-                domainMax = new double[n];
-                for (int i = 0; i < n; i++) {
-                    domainMax[i] = Double.MIN_VALUE;
-                }
-            }
-            Double[] dataArr = new Double[n];
-            for (int i = 1; i < rowArr.length; i++) {
-                dataArr[i - 1] = Double.parseDouble(rowArr[i]);
-                if (dataArr[i - 1] > domainMax[i - 1]) {
-                    domainMax[i - 1] = dataArr[i - 1];
-                }
-            }
-            data.put(rowArr[0], dataArr);
-            row = csv.readLine();
+    public void run(boolean initRandomClusters, List<double[]> totalNormAvg, double minimunTotal,
+            List<List<String>[]> G) throws IOException {
+        KmeansND algorithm = new KmeansND(data, clustersNumber, n);
+        if (initRandomClusters) {
+            algorithm.initClusters();
+        } else {
+            algorithm.initMeans();
         }
-        for (Double[] vector : data.values()) {
-            for (int i = 0; i < n; i++) {
-                vector[i] = vector[i] / domainMax[i]; // normalizza i dati -> tutto è adesso fra 0 e 1
-            }
-        }
+        totalNormAvg.add(Integer.parseInt(this.getName()), algorithm.executeKMeans());
 
-        int clustersNumber = Integer.parseInt(positionals.get(1)); // args[0]
-        Random r = new Random();
-        c = 0;
-        double minimunTotal = Double.MAX_VALUE;
-        List<String>[] G = null;
-        while (c < 100) {
-            KmeansND algorithm = new KmeansND(data, clustersNumber, n);
-            if (initRandomClusters) {
-                algorithm.initClusters();
-            } else {
-                algorithm.initMeans();
-            }
-            double[] totalNormAvg = algorithm.executeKMeans();
+        /*
+         * double total = 0; for (double avg : totalNormAvg) { total = total + avg; }
+         */
 
-            double total = 0;
-            for (double avg : totalNormAvg) {
-                total = total + avg;
-            }
-            if (minimunTotal > total) {
-                minimunTotal = total;
-                G = algorithm.getClusters();
-            }
-            c++;
-        }
-        System.out.println(formatTable(G));
-        // formatTable ogni tanto potrebbe scazzare l'impaginazione (soprattutto per datasetProva.csv cha ha nomi di
-        // una sola lettera) in caso meglio usare printList (però con lui non si vedono bene i cluster di test_reale.csv)
-        // comunque la cosa che ci faceva vedere tutti i cluster in ordine era un errore in format table, che ho più
-        // o meno risotto
-//        printList(G);
+        G.add(Integer.parseInt(this.getName()), algorithm.getClusters());// prende il nome del thread e ci mette il
+                                                                         // nuovo cluster
     }
 
     public static List<String>[] kmeans(Map<String, Double[]> data, double[][] initialMeans, double[] totalNormAvg) {
@@ -273,86 +213,6 @@ public class KmeansND {
             }
             System.out.println(".");
         }
-    }
-
-    public static void printAnyThing(Object S) {
-        if (S instanceof Object[]) {
-            Object[] oArr = (Object[]) S;
-            for (Object o : oArr) {
-                printAnyThing(o);
-                System.out.print(",");
-            }
-            System.out.println(".");
-            System.out.println();
-        } else if (S instanceof Iterable) {
-            Iterable<Object> oIter = (Iterable<Object>) S;
-            for (Object o : oIter) {
-                printAnyThing(o);
-                System.out.print(",");
-            }
-            System.out.println(".");
-            System.out.println();
-        } else {
-            System.out.print(S.toString());
-        }
-    }
-
-    public static String formatTable(Object o) {
-        StringBuilder sb = new StringBuilder();
-        Formatter form = new Formatter(sb);
-        if (o instanceof Object[]) {
-            Object[] arr = (Object[]) o;
-            List<List<String>> content = new ArrayList<>(arr.length);
-            for (Object e : arr) {
-                content.add(mapsObj(e));
-            }
-            for (int i = 0; i < arr.length; i++) {
-                form.format("\t%d", i);
-            }
-            sb.append("\n");
-            int j = 0;
-            boolean next = true;
-            String last = null;
-            while (next) {
-                next = false;
-                for (int i = 0; i < arr.length; i++) {
-                    List<String> col = content.get(i);
-                    if (j < col.size()) {
-                        String elem = col.get(j);
-                        last = elem;
-                        form.format("\t%s", elem);
-                        if (j + 1 < col.size()) {
-                            next = true;
-                        }
-                    } else {
-                        if (last != null) { // questo aiuta
-                            // ma questa funzione è ancora buggata...pace serve solo a stampare
-                            int offset = last.length() / 8 + 1;
-                            for (int h = 0; h < offset; h++) {
-                                form.format("\t");
-                            }
-                        } else {
-                            form.format("\t\t");
-                        }
-                    }
-                }
-                sb.append("\n");
-                j++;
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    public static List<String> mapsObj(Object o) {
-        List<String> lt = new LinkedList<>();
-        if (o instanceof Iterable) {
-            Iterable<Object> it = (Iterable<Object>) o;
-            for (Object e : it) {
-                lt.add(e.toString());
-            }
-        }
-        return lt;
     }
 
     public static double norm(Double[] a, double[] b) {
---
+++ kmeans/src/Main.java
@@ -0,0 +1,142 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int c = 0;
+        List<String> positionals = new LinkedList<String>();
+        boolean initRandomClusters = false;
+        while (c < args.length) {
+            if ("-initClusters".equals(args[c])) {
+                initRandomClusters = true;
+            } else {
+                positionals.add(args[c]);
+            }
+            c++;
+        }
+        BufferedReader csv = null;
+        try {
+            csv = new BufferedReader(new FileReader(positionals.get(0)));
+        } catch (FileNotFoundException e) {
+            System.err.println("given path does not exists");
+            System.exit(1);
+        }
+
+        String row = csv.readLine();
+        HashMap<String, Double[]> data = new HashMap<>();
+        double[] domainMax = null;
+        int n = -1;
+        while (row != null) {
+            String[] rowArr = row.split(";");
+            if (n == -1) {
+                n = rowArr.length - 1;
+                domainMax = new double[n];
+                for (int i = 0; i < n; i++) {
+                    domainMax[i] = Double.MIN_VALUE;
+                }
+            }
+            Double[] dataArr = new Double[n];
+            for (int i = 1; i < rowArr.length; i++) {
+                dataArr[i - 1] = Double.parseDouble(rowArr[i]);
+                if (dataArr[i - 1] > domainMax[i - 1]) {
+                    domainMax[i - 1] = dataArr[i - 1];
+                }
+            }
+            data.put(rowArr[0], dataArr);
+            row = csv.readLine();
+        }
+        for (Double[] vector : data.values()) {
+            for (int i = 0; i < n; i++) {
+                vector[i] = vector[i] / domainMax[i]; // normalizza i dati -> tutto è adesso fra 0 e 1
+            }
+        }
+
+        int clustersNumber = Integer.parseInt(positionals.get(1)); // args[0]
+        Random r = new Random();
+        c = 0;
+        double minimunTotal = Double.MAX_VALUE;
+        List<double[]> totalNormAvg = null;
+        List<List<String>[]> G = null;// da controllare le variabili comuni a tutti i thread se ci si accede in modo
+                                      // giusto, soprattuto G.
+        while (c < 100) {
+            KmeansND t = new KmeansND(data, clustersNumber, n);
+            t.setName(String.valueOf(c));
+            t.run(initRandomClusters, totalNormAvg, minimunTotal, G);
+            c++;
+        }
+        // poi qui si confrontano tutti i valori di G e si prende il migliore.
+        System.out.println(formatTable(G));
+        // formatTable ogni tanto potrebbe scazzare l'impaginazione (soprattutto per
+        // datasetProva.csv cha ha nomi di
+        // una sola lettera) in caso meglio usare printList (però con lui non si vedono
+        // bene i cluster di test_reale.csv)
+        // comunque la cosa che ci faceva vedere tutti i cluster in ordine era un errore
+        // in format table, che ho più
+        // o meno risotto
+        // printList(G);
+    }
+
+    public static String formatTable(Object o) {
+        StringBuilder sb = new StringBuilder();
+        Formatter form = new Formatter(sb);
+        if (o instanceof Object[]) {
+            Object[] arr = (Object[]) o;
+            List<List<String>> content = new ArrayList<>(arr.length);
+            for (Object e : arr) {
+                content.add(mapsObj(e));
+            }
+            for (int i = 0; i < arr.length; i++) {
+                form.format("\t%d", i);
+            }
+            sb.append("\n");
+            int j = 0;
+            boolean next = true;
+            String last = null;
+            while (next) {
+                next = false;
+                for (int i = 0; i < arr.length; i++) {
+                    List<String> col = content.get(i);
+                    if (j < col.size()) {
+                        String elem = col.get(j);
+                        last = elem;
+                        form.format("\t%s", elem);
+                        if (j + 1 < col.size()) {
+                            next = true;
+                        }
+                    } else {
+                        if (last != null) { // questo aiuta
+                            // ma questa funzione è ancora buggata...pace serve solo a stampare
+                            int offset = last.length() / 8 + 1;
+                            for (int h = 0; h < offset; h++) {
+                                form.format("\t");
+                            }
+                        } else {
+                            form.format("\t\t");
+                        }
+                    }
+                }
+                sb.append("\n");
+                j++;
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static List<String> mapsObj(Object o) {
+        List<String> lt = new LinkedList<>();
+        if (o instanceof Iterable) {
+            Iterable<Object> it = (Iterable<Object>) o;
+            for (Object e : it) {
+                lt.add(e.toString());
+            }
+        }
+        return lt;
+    }
+
+}
